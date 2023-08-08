32166e8893972650abdb637b5d39f51839aa96ff
ibrahim <solaadams423@gmail.com>
2023-08-08 13:06:44 +0100

    created user route and called it in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,22 @@ const express = require('express');
 const mongoose = require("mongoose")
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//
+app.use("/api/users", userRoute);
+
+//routes
+app.get('/', (req, res)=>{
+    res.send("Welcome to homepage")
+});
 
 const PORT = process.env.PORT || 5500;
 
